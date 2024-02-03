0b71cc4524a1aae00d47605e7bfec128e3a0b8b2
Grace <yintingchancytg@gmail.com>
2024-02-03 19:07:26 +0100

    Created fontSizes variables

--- src/theme/variables/fontSizes.ts
+++ src/theme/variables/fontSizes.ts
@@ -1,3 +1,10 @@
-const fontSizes = {};
+const fontSizes = {
+  ant: '0.9375rem;',     // 14px
+  mouse: '1.25rem',      // 18px
+  cat: '1.5rem',         // 24px
+  raccoon: '1.875rem',   // 30px
+  eagle: '2.25rem',      // 36px
+  whale: '3rem'          // 48px
+};
 
 export default fontSizes;
