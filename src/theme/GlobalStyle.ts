c6c915a1b7a2293fb339efff849179a3d9e55a9a
Grace <yintingchancytg@gmail.com>
2024-02-03 19:08:20 +0100

    Added default font-size

--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -6,6 +6,7 @@ const GlobalStyle = createGlobalStyle`
     border: 0;
     color: ${({ theme }) => theme.colors.base};
     font-family: 'Helvetica', 'Arial', sans-serif;
+    font-size: ${({ theme }) => theme.fontSizes.mouse};
     line-height: 1;
     margin: 0;
     padding: 0;
