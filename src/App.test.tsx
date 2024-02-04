ad03bf556413799fdccd367cb849e61d73873bf5
Grace <yintingchancytg@gmail.com>
2024-02-04 14:10:37 +0100

    Added Home route

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 test('renders App component', () => {
-  render(<App />);
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
   const appElement = screen.getByTestId(/App/i);
   expect(appElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './theme/theme';
 import GlobalStyle from './theme/GlobalStyle';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div className="App" data-testid="App" />
+
+      <div className="App" data-testid="App">
+        <Routes>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
     </ThemeProvider>
   );
 }
