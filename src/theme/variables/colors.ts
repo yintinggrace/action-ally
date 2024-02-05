4aab628b15e6b5737a7e0b490d76d7f001a72c61
Grace <yintingchancytg@gmail.com>
2024-02-05 20:59:20 +0100

    Added colors

--- src/theme/variables/colors.ts
+++ src/theme/variables/colors.ts
@@ -1,12 +1,43 @@
 const colors = {
-  // Blacks
+  // Black
   black: 'rgb(0, 0, 0)',
-  base: 'rgb(0, 0, 0)',
 
-  // Whites
-  white: 'rgb(255, 255, 255)'
+  // Gray
+  darkGray: 'rgb(89, 89, 89)',
+  mediumGray: 'rgb(206, 206, 206)',
+  lightGray: 'rgb(236, 236, 236)',
 
-  // Colors
+  // White
+  white: 'rgb(255, 255, 255)',
+
+  // Blue
+  royalBlue: 'rgb(71, 81, 150)',
+  deepBlue: 'rgb(82, 91, 159)',
+  skyBlue: 'rgb(93, 183, 234)',
+
+  // Icon Background Colors
+  coralPink: 'rgb(255, 211, 209)',
+  peach: 'rgb(255, 225, 180)',
+  lemonYellow: 'rgb(255, 235, 132)',
+  mintGreen: 'rgb(195, 255, 167)',
+  babyBlue: 'rgb(167, 255, 244)',
+  lavender: 'rgb(184, 167, 255)',
+  orchid: 'rgb(253, 167, 255)',
+  terraCotta: 'rgb(212, 87, 79)',
+  silver: 'rgb(182, 182, 182)',
+  copper: 'rgb(196, 132, 70)',
+
+  // Icon Colors
+  terraCottaIcon: 'rgb(212, 87, 79)',
+  sunflower: 'rgb(255, 158, 12)',
+  maize: 'rgb(253, 227, 90)',
+  fernGreen: 'rgb(77, 164, 36)',
+  tealBlue: 'rgb(60, 221, 201)',
+  blueViolet: 'rgb(128, 102, 231)',
+  heliotrope: 'rgb(217, 91, 220)',
+  russet: 'rgb(120, 49, 45)',
+  slateGray: 'rgb(130, 130, 130)',
+  rawUmber: 'rgb(126, 85, 46)'
 };
 
 export default colors;
