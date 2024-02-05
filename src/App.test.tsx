2b24744b1c71d660a9ad369e97cae703a0e8cd20
Grace <yintingchancytg@gmail.com>
2024-02-05 20:55:49 +0100

    Implemented BrowserRouter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 test('renders App component', () => {
-  render(
-    <Router>
-      <App />
-    </Router>
-  );
+  render(<App />);
   const appElement = screen.getByTestId(/App/i);
   expect(appElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import GlobalStyle from './theme/GlobalStyle';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 const App = () => {
@@ -7,11 +7,13 @@ const App = () => {
     <>
       <GlobalStyle />
 
-      <div className="App" data-testid="App">
-        <Routes>
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </div>
+      <Router>
+        <div className="App" data-testid="App">
+          <Routes>
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 };
