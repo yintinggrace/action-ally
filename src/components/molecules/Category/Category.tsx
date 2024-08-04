45359a33f97fb5773f576c8eabfb31d3cdf913e9
Grace <yintingchancytg@gmail.com>
2024-08-04 16:52:53 +0200

    Implemented getting and rendering category

---
+++ src/components/molecules/Category/Category.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import theme from '../../../theme/theme';
+import Text from '../../atoms/Text/Text';
+import Icon from '../../atoms/Icon/Icon';
+
+interface CategoryItem {
+  name: string;
+  icon: string;
+  backgroundColor: string;
+  iconColor: string;
+}
+
+interface CategoryProps {
+  category: CategoryItem;
+}
+
+const SCategoryWrapper = styled.li`
+  height: ${theme.space(44)};
+  width: ${theme.space(38)};
+  background-color: ${theme.colors.white};
+  border-radius: ${theme.space(2)};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const SCategoryInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: ${theme.space(2)};
+`;
+
+const SIcon = styled(Icon)<{ iconColor: string, backgroundColor: string }>`
+  font-size: ${theme.fontSizes.cat};
+  color: ${({ iconColor }) => iconColor};
+  padding: ${theme.space(2.5)};
+  margin-bottom: ${theme.space(2)};
+  border-radius: 50%;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`;
+
+const SText = styled(Text)`
+  font-size: ${theme.fontSizes.ant};
+  color: ${theme.colors.darkGray};
+`;
+
+const Category: React.FC<CategoryProps> = ({ category }) => {
+  return (
+    <SCategoryWrapper>
+      <SCategoryInfo>
+        <SIcon
+          name={category.icon}
+          iconColor={category.iconColor}
+          backgroundColor={category.backgroundColor}
+        />
+        <SText>{category.name}</SText>
+      </SCategoryInfo>
+    </SCategoryWrapper>
+  );
+};
+
+export default Category;
--- src/components/organisms/CategoryList/CategoryList.tsx
+++ src/components/organisms/CategoryList/CategoryList.tsx
@@ -1,23 +1,43 @@
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import theme from '../../../theme/theme';
 import string from '../../../string';
 import Heading from '../../atoms/Heading/Heading';
 import AddCategoryButton from '../AddCategoryButton/AddCategoryButton';
+import Category from '../../molecules/Category/Category';
 
 const SCategoriesWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
   width: 100%;
   gap: ${theme.space(7.5)};
 `;
 
 const ContentWrapper = styled.div`
   display: flex;
-  justify-content: flex-start;
+  flex-wrap: wrap;
+  justify-content: center;
   gap: ${theme.space(7.5)};
 `;
 
+interface Category {
+  name: string;
+  icon: string;
+  backgroundColor: string;
+  iconColor: string;
+}
+
 const CategoryList = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    const storedCategories = JSON.parse(localStorage.getItem('categories') || '[]');
+    setCategories(storedCategories);
+  }, [location]);
+
   return (
     <SCategoriesWrapper>
       <Heading type="h3">
@@ -25,6 +45,13 @@ const CategoryList = () => {
       </Heading>
 
       <ContentWrapper>
+        {categories.map((category, index) => (
+          <Category
+            key={index}
+            category={category}
+          />
+        ))}
+
         <AddCategoryButton />
       </ContentWrapper>
     </SCategoriesWrapper>
