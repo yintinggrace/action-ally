bb1b14f5cd3b99249c805aeece6da7e867708935
Grace <yintingchancytg@gmail.com>
2024-08-22 20:37:40 +0200

    Updated CategoryList styling

--- src/components/organisms/CategoryList/CategoryList.tsx
+++ src/components/organisms/CategoryList/CategoryList.tsx
@@ -12,7 +12,12 @@ const SCategoriesWrapper = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   width: 100%;
-  gap: ${theme.space(7.5)};
+  gap: ${theme.space(6)};
+  max-width: 600px;
+  background-color: ${theme.colors.whiteTransparent};
+  border-radius: ${theme.space(5)};
+  padding: ${theme.space(6)};
+  box-sizing: border-box;
 `;
 
 const ContentWrapper = styled.div`
