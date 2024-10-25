80c59bada0fdc2c1b111cc3c862916e3c62789b1
Grace <yintingchancytg@gmail.com>
2024-10-25 18:53:03 +0200

    Refactored task item as grid

--- src/components/atoms/Checkbox/Checkbox.tsx
+++ src/components/atoms/Checkbox/Checkbox.tsx
@@ -2,6 +2,7 @@ import theme from '../../../theme/theme';
 import styled from 'styled-components';
 
 interface CheckboxProps {
+  className?: string;
   checked?: boolean;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   name?: string;
@@ -18,13 +19,14 @@ const SInput = styled.input`
   }
 `;
 
-const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange, name }) => {
+const Checkbox: React.FC<CheckboxProps> = ({ className, checked, onChange, name }) => {
   return (
     <SInput
       type="checkbox"
       checked={checked}
       onChange={onChange}
       name={name}
+      className={className}
     />
   );
 }
--- src/components/molecules/TaskItem/TaskItem.tsx
+++ src/components/molecules/TaskItem/TaskItem.tsx
@@ -17,30 +17,26 @@ interface TaskItemProps {
 const SLi = styled.li`
   padding: ${theme.space(3)};
   border-bottom: 1px solid ${theme.colors.mediumGray};
-  display: flex;
-  align-items: start;
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  align-items: center;
+  gap: 0 ${theme.space(5)};
 `;
 
-const SDetails = styled.div`
-  line-height: ${theme.space(6)};
-  padding: 0 ${theme.space(6)};
-  width: 90%;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  gap: ${theme.space(1)};
+const SCheckbox = styled(Checkbox)`
+  grid-column: 1;
+  grid-row: 1;
 `;
 
 const SHeading = styled(Heading)`
+  grid-column: 2;
+  grid-row: 1;
   color: ${theme.colors.black};
 `;
 
-const SText = styled(Text)`
-  font-size: ${theme.fontSizes.ant};
-  font-weight: normal;
-  color: ${theme.colors.darkGray};
-  margin-top: ${theme.space(1)};
-  font-style: italic;
+const SIcons = styled.div`
+  grid-column: 3;
+  grid-row: 1;
 `;
 
 const SIcon = styled(Icon)`
@@ -55,6 +51,24 @@ const SIcon = styled(Icon)`
   &:active {
     color: ${theme.colors.mediumBlack};
   }
+`;
+
+const SDetails = styled.div`
+  line-height: ${theme.space(6)};
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.space(1)};
+  grid-column: 2;
+  grid-row: 2;
+`;
+
+const SText = styled(Text)`
+  font-size: ${theme.fontSizes.ant};
+  font-weight: normal;
+  color: ${theme.colors.darkGray};
+  margin-top: ${theme.space(1)};
+  font-style: italic;
+  line-height: ${theme.space(6)};
 `;
 
 const TaskItem: React.FC<TaskItemProps> = ({
@@ -77,32 +91,38 @@ const TaskItem: React.FC<TaskItemProps> = ({
 
   return (
     <SLi>
-      <Checkbox
+      {/* Row 1 */}
+      <SCheckbox
         checked={false}
         onChange={handleRemoveTask}
       />
 
-      <SDetails>
-        <SHeading type="h3">{task.name}</SHeading>
+      <SHeading type="h3">{task.name}</SHeading>
 
-        {task.info && (
-          <SText>{task.info}</SText>
+      <SIcons>
+        <SIcon
+          name="fas fa-pen"
+          onClick={() => handleEditClick(task)}
+        />
+
+        <SIcon
+          name="fas fa-trash"
+          onClick={handleRemoveTask}
+        />
+      </SIcons>
+
+      {(task.info || task.location) && (
+        <SDetails>
+          {task.info && (
+            <SText>{task.info}</SText>
+          )}
+
+          {/* Row 3 */}
+          {task.location && (
+            <SText>{task.location}</SText>
+          )}
+        </SDetails>
         )}
-
-        {task.location && (
-          <SText>{task.location}</SText>
-        )}
-      </SDetails>
-
-      <SIcon
-        name="fas fa-pen"
-        onClick={() => handleEditClick(task)}
-      />
-
-      <SIcon
-        name="fas fa-trash"
-        onClick={handleRemoveTask}
-      />
     </SLi>
   )
 }
