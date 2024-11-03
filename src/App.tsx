4f9a4044bd7cb1999d8fac6ae10b91c7fc1404d2
Grace <yintingchancytg@gmail.com>
2024-11-03 14:42:23 +0100

    Refactored basename

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
     <>
       <GlobalStyle />
 
-      <Router basename={process ? process.env.REACT_APP_BASENAME : '/'}>
+      <Router basename={process.env.REACT_APP_BASENAME || '/'}>
         <div className="App" data-testid="App">
           <Routes>
             <Route path="/save-task" element={<SaveTask />} />
